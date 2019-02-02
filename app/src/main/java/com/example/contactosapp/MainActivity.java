1afb5fcb37eef5f6c232e87aaf9a9fd77f1fa779
andres <andres97gt@gmail.com>
2019-02-01 18:45:31 -0600

    Initial commit

---
+++ app/src/main/java/com/example/contactosapp/MainActivity.java
@@ -0,0 +1,217 @@
+package com.example.contactosapp;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int PICK_CONTACT_REQUEST = 1;
+    private final int PERMISSIONS_REQUEST_READ_CONTACT = 100;
+    private Uri contactoUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED){
+
+            requestPermissions(
+                    new String[]{Manifest.permission.READ_CONTACTS},
+                    PERMISSIONS_REQUEST_READ_CONTACT
+                    );
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSIONS_REQUEST_READ_CONTACT:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    return;
+                } else {
+                    finish();
+                }
+                return;
+        }
+    }
+
+    public void initSeleccionarContacto(View v) {
+        Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(i, PICK_CONTACT_REQUEST);
+    }
+
+    private void recibirContacto(Uri uri){
+        TextView nombre = (TextView) findViewById(R.id.nombreContacto);
+        TextView telefono = (TextView) findViewById(R.id.telefonoContacto);
+        TextView correo = (TextView) findViewById(R.id.correoContacto);
+        ImageView foto = (ImageView) findViewById(R.id.fotoContacto);
+
+        nombre.setText(getNombre(uri));
+        telefono.setText(getTelefono(uri));
+        correo.setText(getCorreo(uri));
+        foto.setImageBitmap(getFoto(uri));
+    }
+
+    private String getNombre(Uri uri){
+        String nombre = null;
+        ContentResolver contentResolver = getContentResolver();
+        Cursor c = contentResolver.query(
+                uri,
+                new String[]{ContactsContract.Contacts.DISPLAY_NAME},
+                null,
+                null,
+                null
+        );
+        if(c.moveToFirst()){
+            nombre = c.getString(0);
+        }
+        c.close();
+        return nombre;
+    }
+
+    private String getTelefono(Uri uri){
+        String id = null;
+        String telefono = null;
+        Cursor contactoCursor = getContentResolver().query(
+                uri,
+                new String[]{ContactsContract.Contacts._ID},
+                null,
+                null,
+                null
+        );
+        if(contactoCursor.moveToFirst()){
+            id = contactoCursor.getString(0);
+        }
+        contactoCursor.close();
+
+        String selectionArgs = ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ? AND " +
+                ContactsContract.CommonDataKinds.Phone.TYPE + " = " +
+                ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE;
+
+        Cursor telefonoCursor = getContentResolver().query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
+                selectionArgs,
+                new String[]{id},
+                null
+        );
+
+        if(telefonoCursor.moveToFirst()){
+            telefono = telefonoCursor.getString(0);
+        }
+
+        telefonoCursor.close();
+
+        return telefono;
+    }
+
+    private Bitmap getFoto(Uri uri){
+        String id = null;
+        Bitmap foto = null;
+        Cursor contactoCursor = getContentResolver().query(
+                uri,
+                new String[]{ContactsContract.Contacts._ID},
+                null,
+                null,
+                null
+        );
+        if(contactoCursor.moveToFirst()){
+            id = contactoCursor.getString(0);
+        }
+        contactoCursor.close();
+
+        try {
+
+            Uri contactoUri = ContentUris.withAppendedId(
+                    ContactsContract.Contacts.CONTENT_URI,
+                    Long.parseLong(id)
+            );
+            InputStream input = ContactsContract.Contacts.openContactPhotoInputStream(
+                    getContentResolver(),
+                    contactoUri
+            );
+
+            if (input != null){
+                foto = BitmapFactory.decodeStream(input);
+                input.close();
+            }
+
+        }catch (IOException ioe){
+            // Manejamos algun error de entrada y salida
+        }
+
+        return foto;
+    }
+
+    private String getCorreo(Uri uri){
+        String id = null;
+        String correo = null;
+        Cursor contactoCursor = getContentResolver().query(
+                uri,
+                new String[]{ContactsContract.Contacts._ID},
+                null,
+                null,
+                null
+        );
+        if(contactoCursor.moveToFirst()){
+            id = contactoCursor.getString(0);
+        }
+        contactoCursor.close();
+
+        String selectionArgs = ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?";
+        Cursor correoCursor = getContentResolver().query(
+                ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                new String[]{ContactsContract.CommonDataKinds.Email.DATA1},
+                selectionArgs,
+                new String[]{id},
+                null
+        );
+
+        if(correoCursor.moveToFirst()){
+            correo = correoCursor.getString(0);
+        }
+
+        correoCursor.close();
+
+        return correo;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(requestCode == PICK_CONTACT_REQUEST){
+            if(resultCode == RESULT_OK){
+                contactoUri = data.getData();
+
+                recibirContacto(contactoUri);
+            }
+        }
+
+    }
+}
